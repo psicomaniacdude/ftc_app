2d54148bec6160eca11ac47e9f5933abd6f5f12c
psicomaniacdude <32318142+psicomaniacdude@users.noreply.github.com>
2017-09-30 13:46:02 -0700

    Added Code to begin testing new base

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewMecanumBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewMecanumBase.java
@@ -1,7 +1,56 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+
 /**
  * Created by RoboPir8s on 9/27/2017.
  */
-public class NewMecanumBase {
+public class NewMecanumBase extends OpMode {
+
+
+    //Drive Motors
+    DcMotor TopLeft;
+    DcMotor TopRight;
+    DcMotor BotLeft;
+    DcMotor BotRight;
+
+    //Conveyor Motors
+    DcMotor Con1;
+    DcMotor Con2;
+    @Override
+    public void init(){
+        TopLeft = hardwareMap.dcMotor.get("topleft");
+        TopRight = hardwareMap.dcMotor.get("topright");
+        BotLeft = hardwareMap.dcMotor.get("botleft");
+        BotRight = hardwareMap.dcMotor.get("botright");
+
+        Con1 = hardwareMap.dcMotor.get("con1");
+        Con2 = hardwareMap.dcMotor.get("con2");
+    }
+
+    @Override
+    public void loop(){
+
+        //Set up Holonomic drive
+        double Throttle = -gamepad1.left_stick_y;
+        double Holonomic = gamepad1.left_stick_x;
+        double Direction = gamepad1.right_stick_x;
+
+        double TopL = Throttle + Holonomic + Direction;
+        double TopR = -Throttle + Holonomic + Direction;
+        double BotL = Throttle - Holonomic + Direction;
+        double BotR = -Throttle - Holonomic + Direction;
+
+        TopL = Range.clip(TopL, -1, 1);
+        TopR = Range.clip(TopR,-1,1);
+        BotL = Range.clip(BotL,-1,1);
+        BotR = Range.clip(BotR,-1,1);
+        //Set motor power to gamepad values
+        TopLeft.setPower(TopL);
+        TopRight.setPower(TopR);
+        BotLeft.setPower(BotL);
+        BotRight.setPower(BotR);
+    }
 }
