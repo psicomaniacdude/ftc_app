c1b89c13468b8a55407b42d2b9957b8a6d3f773f
psicomaniacdude <32318142+psicomaniacdude@users.noreply.github.com>
2017-12-11 16:25:37 -0800

    Copyed the code straight from the YogaBlue program. Something was copied over from when I was writing it down the first time.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
@@ -1,155 +1,357 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.I2cAddr;
-import com.qualcomm.robotcore.hardware.I2cDevice;
-import com.qualcomm.robotcore.hardware.I2cDeviceSynch;
-import com.qualcomm.robotcore.hardware.I2cDeviceSynchImpl;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
 
 import java.util.Vector;
-
 /**
- * Created by RoboPir8s on 9/30/2017.
+ * Created by RoboPir8s on 1/26/2017
  */
-public class AutoTest extends LinearOpMode {
+@Autonomous(name = "Yoga_Blue", group ="Autonomous")
+public class Yoga_Blue extends LinearOpMode
+{
+    DcMotor topLeft,topRight,botRight,botLeft,RightCollectMotor,LeftCollectMotor;//catapultMotor;
+    Servo LL, LR, RL, RR, ballStop;
+    OpticalDistanceSensor ODSSensor;
 
-    DcMotor TopLeft, TopRight, BotLeft, BotRight;
-    //We declare the Color Sensor as an I2c device so that we can set the I2c address, and then read the color
-    //number value from the sensor. The color number is more useful than using the colorsensor.red command, because
-    //it allows us to have to look at only one number to determine the color being viewed.
-    I2cDevice JewelSensor;
-    //The DeviceSynch makes it easier to read the values we want from the I2c Addresses.
-    I2cDeviceSynch JewelReader;
-
-    @Override
     public void runOpMode() throws InterruptedException {
+        int startDelay = 1000;
         Vector<Integer> steps = new Vector<>(1);
         Vector<String> stringSteps = new Vector<>(1);
         instructions go = new instructions();
 
-        TopLeft = hardwareMap.dcMotor.get("topleft");
-        TopRight = hardwareMap.dcMotor.get("topright");
-        BotLeft = hardwareMap.dcMotor.get("botleft");
-        BotRight = hardwareMap.dcMotor.get("botright");
+        //Get hardware maps for the motors
+        topLeft = hardwareMap.dcMotor.get("topleft");
+        topRight = hardwareMap.dcMotor.get("topright");
+        botLeft = hardwareMap.dcMotor.get("botleft");
+        botRight = hardwareMap.dcMotor.get("botright");
+        RightCollectMotor = hardwareMap.dcMotor.get("collectright");
+        LeftCollectMotor = hardwareMap.dcMotor.get("collectleft");
 
-        JewelSensor = hardwareMap.i2cDevice.get("jewel");
-        JewelReader = new I2cDeviceSynchImpl(JewelSensor, I2cAddr.create8bit(0x3c), false);
 
-
+        /*catapultMotor = hardwareMap.dcMotor.get("catapult");
+        LL = hardwareMap.servo.get("leftl");
+        LR = hardwareMap.servo.get("leftr");
+        RL = hardwareMap.servo.get("rightl");
+        RR = hardwareMap.servo.get("rightr");
+        ballStop = hardwareMap.servo.get("ballstop");
+        ODSSensor = hardwareMap.opticalDistanceSensor.get("ods");
+        LR.setDirection(Servo.Direction.REVERSE);
+        RR.setDirection(Servo.Direction.REVERSE);
+        LL.setPosition(0);
+        LR.setPosition(0);
+        RL.setPosition(0);
+        RR.setPosition(0);
+        ballStop.setPosition(.75);
+        */
+//        double LightReading = ODSSensor.getLightDetected();
+//        double PowerLeft = ((.5-ODSSensor.getLightDetected())*.3);
+//        double PowerRight = (ODSSensor.getLightDetected() -.255)*.3;
         while(!isStarted())
         {
-            boolean autoPressed = false;
-            if(gamepad1.a && !autoPressed)
+            telemetry.addData("Start Delay: ", startDelay / 1000);
+            telemetry.addData("Steps: ",stringSteps.toString());
+            boolean justPressed = false;
+            if(gamepad1.dpad_up)
             {
-                steps.addElement(1);
-                stringSteps.addElement("Forward Drive");
+                startDelay = Range.clip(startDelay, 1000, 10000);
+                startDelay += 1000;
 
-                while(gamepad1.a)
+                while(gamepad1.dpad_up)
+                {
+                    justPressed = true;
+                }
+            }
+
+            if(gamepad1.dpad_down  && !justPressed)
+            {
+                startDelay = Range.clip(startDelay,1000,10000);
+                startDelay -= 1000;
+
+                while(gamepad1.dpad_down)
+                {
+                    justPressed = true;
+                }
+            }
+            boolean autoPressed = false;
+            if(gamepad1.x && !autoPressed)
+            {
+                steps.addElement(6);
+                stringSteps.addElement("Skooch");
+                while(gamepad1.x)
                 {
                     autoPressed = true;
                 }
             }
-            waitForStart();
-
-            for(int step = 0; step < steps.size(); step++) //This is where we define each case
+            if (gamepad1.y && autoPressed)
             {
-                switch (steps.get(step))
+                steps.addElement(5);
+                stringSteps.addElement("Button Pushers");
+                while(gamepad1.y)
                 {
-                    case 1://shoot
+                    autoPressed = true;
+                }
 
-                        go.Jewel_Read_Red(5000);
-                        break;
-                    /*case 2://pos 2
+            }
+            if(gamepad1.b  && !autoPressed) {
+                steps.addElement(3);
+                stringSteps.addElement("Pos 1");
 
-                        go.spinLeft(1, 1200);
-                        go.forward(1, 500);
-                        go.spinRight(1, 1200);
-                        go.forward(1, 1000);
-                        break;
-                    case 3://pos 1
-
-                        go.forward(-1,225);
-                        go.spinLeft(1, 1500);
-                        go.stop(1000);
-                        go.forward(1, 2500);
-                        go.stop(3000);
-                        break;
-                    case 4:
-                        long duration =System.currentTimeMillis();
-                        while (!exit) {
-                            go.lineFollow();
-                            if (System.currentTimeMillis() - duration < 50000) exit =true;
-                        }
-                        break;
-                    case 5:
-                        step = steps.size();//needs to be updated will button push program do this last
-                        break;
-                    case 6:
-                        go.Skooch(1200);
-                        break;
-                    case 99:
-                        step = steps.size();
-                        break;*/
+                while(gamepad1.b)
+                {
+                    autoPressed = true;
                 }
             }
 
+            if (gamepad1.a) {
+                steps.addElement(1);
+                stringSteps.addElement("Shoot");
+
+                while (gamepad1.a) {
+                    autoPressed = true;
+                }
+            }
+            if(gamepad1.dpad_left && !autoPressed)
+            {
+                steps.addElement(4);
+                stringSteps.addElement("Line Follower");
+                while(gamepad1.dpad_left) {
+                    autoPressed = true;
+                }
+            }
+
+            if (gamepad1.start &&!autoPressed) {//removes the last element from the instructions
+                steps.removeElement(steps.lastElement());
+                stringSteps.remove(stringSteps.lastElement());
+                while(gamepad1.start)
+                {
+                    autoPressed = true;
+                }
+            }
+
+
+            while(gamepad1.left_trigger > .5) {
+
+                if (gamepad1.b && !autoPressed) {
+                    steps.addElement(2);
+                    stringSteps.addElement("Pos 2");
+
+                    while (gamepad1.b) {
+                        autoPressed = true;
+                    }
+
+                }
+                if (gamepad1.a) {
+                    steps.addElement(1);
+                    stringSteps.addElement("Shoot");
+
+                    while (gamepad1.a) {
+                        autoPressed = true;
+                    }
+                }
+
+                if (gamepad1.y && autoPressed)
+                {
+                    steps.addElement(5);
+                    stringSteps.addElement("Button Pushers");
+                    while(gamepad1.y)
+                    {
+                        autoPressed = true;
+                    }
+
+                }
+                if(gamepad1.dpad_left && !autoPressed)
+                {
+                    steps.addElement(4);
+                    stringSteps.addElement("Line Follower");
+                    while(gamepad1.dpad_left) {
+                        autoPressed = true;
+                    }
+                }
+
+                if (gamepad1.left_trigger > .9 && gamepad1.left_bumper && gamepad1.right_trigger > .9 && gamepad1.right_bumper && !autoPressed) {
+                    steps.addElement(99);
+                    stringSteps.addElement("TESTING");
+
+                    while (gamepad1.left_trigger > .9 && gamepad1.left_bumper && gamepad1.right_trigger > .9 && gamepad1.right_bumper) {
+                        autoPressed = true;
+                    }
+                }
+                telemetry.update();
+            }
+            telemetry.update();
+        }
+
+        startDelay = Range.clip(startDelay,1000,10000);
+        waitForStart();
+        boolean exit = false;
+        try
+        {
+            Thread.sleep(startDelay);
+        }
+        catch(InterruptedException e)
+        {
+            Thread.currentThread().interrupt();
+        }
+        for(int step = 0; step < steps.size(); step++)
+        {
+            switch (steps.get(step))
+            {
+                case 1://shoot
+
+                    go.forward(1, 1000);
+                    break;
+                case 2://pos 2
+
+                    go.spinLeft(1, 1200);
+                    go.forward(1, 500);
+                    go.spinRight(1, 1200);
+                    go.forward(1, 1000);
+                    break;
+                case 3://pos 1
+
+                    go.forward(-1,225);
+                    go.spinLeft(1, 1500);
+                    go.stop(1000);
+                    go.forward(1, 2500);
+                    go.stop(3000);
+                    break;
+                case 4:
+                    long duration =System.currentTimeMillis();
+                    while (!exit) {
+                        go.lineFollow();
+                        if (System.currentTimeMillis() - duration < 50000) exit =true;
+                    }
+                    break;
+                case 5:
+                    step = steps.size();//needs to be updated will button push program do this last
+                    break;
+                case 6:
+                    go.Skooch(1200);
+                    break;
+                case 99:
+                    step = steps.size();
+                    break;
+            }
         }
 
     }
-    public class instructions
-    {//This is where we define the steps for each case
 
+    public class instructions {
         private double leftPower, rightPower,power = 1;
-
-        public void Forward_Drive ( long duration, double power)throws InterruptedException
+        public  void buttonPush()// add button pusher here, we neeed to figure out how the bot is going to be position
         {
-            TopLeft.setPower(power);
-            TopRight.setPower(power);
-            BotLeft.setPower(-power);
-            BotRight.setPower(-power);
+            //placeholder
+        }
+        public void Skooch(long duration) throws InterruptedException
+        {
+
+            power *= -1;
+            strafeLeft(power,duration);
+            topRight.setPower(0);
+            topLeft.setPower(0);
+            botLeft.setPower(0);
+            botRight.setPower(0);
+        }
+        public  void lineFollow()
+        {
+            telemetry.addData("light", ODSSensor.getLightDetected());
+            telemetry.update();
+            double motorMotifier = (ODSSensor.getLightDetected() -.255);
+            if (motorMotifier <= 0)
+            {
+                leftPower = motorMotifier * .75;
+                rightPower = .75;
+            }
+            else
+            {
+                leftPower = .75;
+                rightPower = motorMotifier *.75;
+            }
+            Range.clip(leftPower,-1,1);
+            Range.clip(rightPower,-1,1);
+            topLeft.setPower(leftPower);
+            botLeft.setPower(leftPower);
+            topRight.setPower(rightPower);
+            botRight.setPower(rightPower);
+//            topLeft.setPower(.5 + ((ODSSensor.getLightDetected() - .255) * .3));
+//            topRight.setPower(.5 + ((ODSSensor.getLightDetected()+ .255)*.3));
+//            botRight.setPower(.5 + ((ODSSensor.getLightDetected()+ .255)*.3));
+//            botLeft.setPower(.5 + ((ODSSensor.getLightDetected() - .255) * .3));
+        }
+        public void forward(double power, long duration) throws InterruptedException
+        {
+
+            topLeft.setPower(power);
+            topRight.setPower(-power);
+            botLeft.setPower(power);
+            botRight.setPower(-power);
+            Thread.sleep(duration);
+
+        }
+
+        public void spinLeft(double power, long duration) throws InterruptedException
+        {
+            topLeft.setPower(-power);
+            topRight.setPower(-power);
+            botLeft.setPower(-power);
+            botRight.setPower(-power);
+            Thread.sleep(duration);
+
+        }
+
+        public void spinRight(double power, long duration) throws InterruptedException
+        {
+
+
+            topLeft.setPower(power);
+            topRight.setPower(power);
+            botLeft.setPower(power);
+            botRight.setPower(power);
+            Thread.sleep(duration);
+
+        }
+
+        public void stop(long duration) throws InterruptedException
+        {
+
+            topLeft.setPower(0);
+            topRight.setPower(0);
+            botLeft.setPower(0);
+            botRight.setPower(0);
+            Thread.sleep(duration);
+
+        }
+        public void shoot(long duration) throws InterruptedException
+        {
+            //catapultMotor.setPower(-1);
+            Thread.sleep(500);
+            //catapultMotor.setPower(0);
+            ballStop.setPosition(0);
+            Thread.sleep(duration);
+        }
+        public void strafeLeft(double power, long duration) throws InterruptedException
+        {
+
+            topLeft.setPower(power);
+            topRight.setPower(power);
+            botLeft.setPower(-power);
+            botRight.setPower(-power);
             Thread.sleep(duration);
         }
 
-
-        public void Jewel_Read_Red (long duration) throws InterruptedException
-    {
-        //TODO: 10/28/2017 Create a TurnRight, TurnLeft, and PlaceGlyph class.
-        //TODO: 10/28/2017 Research how Vuforia works again, and create a VuforiaClass to replace the PlaceGlyph Class.
-
-        //This is an array, and it is one byte long. We will be storing the values the color
-        //sensor gets in here so that we can view them later.
-        byte[] colorcache;
-        //This saves the values that the JewelReader gives us, which will allow us to make decisions based off
-        //those readings later.
-        colorcache = JewelReader.read(0x04, 1);
-        if (colorcache[0] >= 10 &&colorcache[0] <= 12)
+        public void strafeRight(double power, long duration) throws InterruptedException
         {
-            TopLeft.setPower(power);
-            TopRight.setPower(power);
-            BotLeft.setPower(-power);
-            BotRight.setPower(-power);
-        }else{
-            TopLeft.setPower(-power);
-            TopRight.setPower(-power);
-            BotLeft.setPower(power);
-            BotRight.setPower(power);
-        }
-    }
-        public void Jewel_Read_Blue (long duration) throws InterruptedException{
-            byte[] colorcache;
-            colorcache = JewelReader.read(0x04, 1);
-            if (colorcache[0] >= 1 && colorcache[0] <= 3){
-                TopLeft.setPower(power);
-                TopRight.setPower(power);
-                BotLeft.setPower(-power);
-                BotRight.setPower(-power);
-            }else {
-                TopLeft.setPower(-power);
-                TopRight.setPower(-power);
-                BotLeft.setPower(power);
-                BotRight.setPower(power);
-            }
+
+            topLeft.setPower(-power);
+            topRight.setPower(-power);
+            botLeft.setPower(power);
+            botRight.setPower(power);
+            Thread.sleep(duration);
         }
     }
 }
